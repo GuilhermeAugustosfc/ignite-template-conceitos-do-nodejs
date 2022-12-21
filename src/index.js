a87e0805e4bf3e11ecd841273f17f2a444b9e09e
guilherme augusto <guilherme.silva@ftdata.com>
2022-12-21 13:34:25 -0300

    ajustando testes

--- src/index.js
+++ src/index.js
@@ -16,33 +16,28 @@ const users = [];
 // 	username: 'danilo',
 // 	todos: []
 
-function getUser(username) {
+function getUser(headers) {
+  if (!headers.hasOwnProperty("username")) return false;
+  const username = headers.username;
   return (exitsUsername = users.find((user) => user.username == username));
-}
-
-function commitUser(user) {
-  const index = users.findIndex((_users) => _users.username == user.username);
-  users[index] = user;
 }
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
-  const exitsUsername = getUser(request.headers.username);
+  const exitsUsername = getUser(request.headers);
   if (!exitsUsername) {
-    response.status(400).send("user not exists");
+    return response.status(400).send({ error: "not exits id todo" });
   }
 
   next();
 }
 
 app.post("/users", (request, response) => {
-  const username = request.body.username;
-
-  const validCreate = getUser(username);
-  if (!validCreate) {
-    response.status(404).send("username already exists");
+  const validCreate = getUser(request.body);
+  if (validCreate) {
+    return response.status(400).json({ error: "Usuario already exists" });
   }
-
+  const username = request.body.username;
   const name = request.body.name;
   const newUser = {
     id: uuidv4(), // precisa ser um uuid
@@ -52,17 +47,17 @@ app.post("/users", (request, response) => {
   };
   users.push(newUser);
 
-  return response.json(users);
+  return response.status(201).json({ name: name, username: username });
 });
 
 app.get("/todos", checksExistsUserAccount, (request, response) => {
-  const user = getUser(request.headers.username);
+  const user = getUser(request.headers);
   return response.json(user.todos).status(200);
 });
 
 app.post("/todos", checksExistsUserAccount, (request, response) => {
   // Complete aqui
-  const user = getUser(request.headers.username);
+  const user = getUser(request.headers);
   const title = request.body.title;
   const deadline = request.body.deadline;
   const todo = {
@@ -79,53 +74,45 @@ app.post("/todos", checksExistsUserAccount, (request, response) => {
 
 app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
   // Complete aqui
-  const user = getUser(request.headers.username);
+  const user = getUser(request.headers);
   const id = request.params.id;
 
   const new_title = request.body.title;
   const new_deadline = request.body.deadline;
-
-  try {
+  if (user.todos && user.todos.hasOwnProperty(id)) {
     const todo = user.todos[id];
     todo.title = new_title;
     todo.deadline = new Date(new_deadline);
-    user.todos[id] = todo;
-
-    commitUser(user);
-
-    return response.json(user).status(201);
-  } catch (error) {
-    response.status(404).send("not exits id todo");
+    return response.json(todo).status(201);
+  } else {
+    return response.status(404).send({ error: "not exits id todo" });
   }
 });
 
 app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
   // Complete aqui
 
-  const user = getUser(request.headers.username);
+  const user = getUser(request.headers);
   const id = request.params.id;
-
-  try {
+  if (user.todos.hasOwnProperty(id)) {
     const todo = user.todos[id];
     todo.done = true;
-    commitUser(user);
-    return response.json(user);
-  } catch (error) {
-    response.status(404).send("not exits id todo");
+    return response.json(todo);
+  } else {
+    return response.status(404).send({ error: "not exits id todo" });
   }
 });
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
   // Complete aqui
-  const user = getUser(request.headers.username);
+  const user = getUser(request.headers);
   const id = request.params.id;
 
-  try {
-    delete user.todos[id];
-    commitUser(user);
-    return response.json(user);
-  } catch (error) {
-    response.status(400).send("not exits id todo");
+  if (user.todos && user.todos.hasOwnProperty(id)) {
+    user.todos.splice(id, 1);
+    return response.status(204).json(user);
+  } else {
+    return response.status(404).send({ error: "not exits id todo" });
   }
 });
 
